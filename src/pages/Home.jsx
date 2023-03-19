6aedecbf12d611efcb723263678dea854826f5ee
Vanderson <vandersontavares32@gmail.com>
2023-03-19 11:15:47 -0300

    tailwindcss

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
 
   return (
     <div className="container">
-      <h2 className="title">Melhores Filmes:</h2>
+      <h2 className="title">Melhores Filmesssss:</h2>
       <div className="movies-container">
         {topMovies.length === 0 && <p>Carregando...</p>}
         {topMovies && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)}        
